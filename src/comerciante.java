d7ea6df2c33c8cb77f0a16fadcfca646fc117500
Tiago Chacon <tiagochacon80@gmail.com>
2022-08-01 15:47:07 -0400

    Algorithms and logic exercises

--- src/comerciante.java
+++ src/comerciante.java
@@ -25,8 +25,48 @@ public class comerciante {
             sc.nextLine();
             nomes[i] = sc.nextLine();
             System.out.print("Preço de compra: ");
-            
+            pcompra[i] = sc.nextDouble();
+            System.out.print("Preço de venda: ");
+            pvenda[i] = sc.nextDouble();
         }
+
+        for (int i=0; i<n; i++) {
+            porcentagemLucros[i] = (pvenda[i] - pcompra[i]) / pcompra[i] * 100.00;
+        }
+
+        abaixo = 0;
+        entre = 0;
+        acima = 0;
+
+        for (int i=0; i<n; i++) {
+            if (porcentagemLucros[i] < 10.0) {
+                abaixo++;
+            }
+            else if (porcentagemLucros[i] < 20.0) {
+                entre++;
+            }
+            else {
+                acima++;
+            }
+        }
+
+        vTotalCompra = 0;
+        vTotalVenda = 0;
+
+        for (int i=0; i<n; i++) {
+            vTotalCompra = vTotalCompra + pcompra[i];
+            vTotalVenda = vTotalVenda + pvenda[i];
+        }
+
+        lucroTotal = vTotalVenda - vTotalCompra;
+
+        System.out.println("\nRELATORIO:");
+        System.out.printf("Lucro abaixo de 10%%: %d\n", abaixo);
+        System.out.printf("Lucro entre 10%% e 20%%: %d\n", entre);
+        System.out.printf("Lucro acima de 20%%: %d\n", acima);
+        System.out.printf("Valor total da compra: %.2f\n", vTotalCompra);
+        System.out.printf("Valor total da venda: %.2f\n", vTotalVenda);
+        System.out.printf("Lucro total: %.2f\n", lucroTotal);
 
         sc.close();
     }
