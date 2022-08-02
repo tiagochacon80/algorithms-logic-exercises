550bd0bccb9b372552f8aa657aca6dd61e39a6be
Tiago Chacon <tiagochacon80@gmail.com>
2022-08-02 15:20:26 -0400

    Algorithms and logic exercises

--- src/coordenadas.java
+++ src/coordenadas.java
@@ -19,6 +19,24 @@ public class coordenadas {
         if (x == 0 && y == 0) {
             System.out.println("Origem");
         }
+        else if (x == 0) {
+            System.out.println("Eixo Y");
+        }
+        else if (y == 0) {
+            System.out.println("Eixo X");
+        }
+        else if (x > 0 && y > 0) {
+            System.out.println("Q1");
+        }
+        else if (x < 0 && y > 0) {
+            System.out.println("Q2");
+        }
+        else if (x < 0 && y < 0) {
+            System.out.println("Q3");
+        }
+        else {
+            System.out.println("Q4");
+        }
 
         sc.close();
     }
