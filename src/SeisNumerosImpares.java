436d96e4961ec1c337387d3daf3decb05ddeb880
Tiago Chacon <tiagochacon80@gmail.com>
2022-09-21 21:05:26 -0400

    Java logic challenge

--- src/SeisNumerosImpares.java
+++ src/SeisNumerosImpares.java
@@ -15,11 +15,10 @@ public class SeisNumerosImpares {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Digite um numero inteiro: ");
         int x = sc.nextInt();
-        int cont = 0;
 
+        int cont = 0;
         while (cont < 6) {
             if (x % 2 == 1) {
                 System.out.println(x);
@@ -27,5 +26,6 @@ public class SeisNumerosImpares {
             }
             x++;
         }
+
     }
 }
