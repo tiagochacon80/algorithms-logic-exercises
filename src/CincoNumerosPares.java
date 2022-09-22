b251c9fce9b053c373e165a52188bd9f5aefa310
Tiago Chacon <tiagochacon80@gmail.com>
2022-09-21 21:09:53 -0400

    Java logic challenge

--- src/CincoNumerosPares.java
+++ src/CincoNumerosPares.java
@@ -1,4 +1,4 @@
-// Seis Números Impares
+// Cinco Números Pares
 /*
 Leia um valor inteiro X. Em seguida apresente os 6 valores ímpares consecutivos
 a partir de X, um valor por linha, inclusive o X se for o caso.
@@ -7,25 +7,26 @@ A entrada será um valor inteiro positivo.
 - Saída
 A saída será uma sequência de seis números ímpares.
 */
-
 import java.util.Scanner;
 
-public class SeisNumerosImpares {
+public class CincoNumerosPares {
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         System.out.print("Digite um numero inteiro: ");
-        int x = sc.nextInt();
+        int y = sc.nextInt();
 
         int cont = 0;
-        while (cont < 6) {
-            if (x % 2 == 1) {
-                System.out.println(x);
+        while (cont < 5) {
+            if (y % 2 == 0) {
+                System.out.println(y);
                 cont++;
             }
-            x++;
+            y++;
         }
+
+
 
     }
 }
