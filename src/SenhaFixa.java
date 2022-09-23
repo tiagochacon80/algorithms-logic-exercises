3e395575232da3beb24c4b130a4f5db2f973e2bb
Tiago Chacon <tiagochacon80@gmail.com>
2022-09-23 17:23:37 -0400

    Java logic challenge

--- src/SenhaFixa.java
+++ src/SenhaFixa.java
@@ -19,14 +19,23 @@ public class SenhaFixa {
 
         Scanner sc = new Scanner(System.in);
         System.out.print("Entre com a senha: ");
+
         int senha = sc.nextInt();
 
-        while (senha != 2002) {
+        int tentativas = 1;
+
+        while (tentativas != 3 && senha != 2002) {
             System.out.println("Senha invalida");
-            System.out.print("Tente novamente: ");
+            System.out.print("Tente outra vez: ");
             senha = sc.nextInt();
+            tentativas++;
+            if (tentativas == 3) {
+                System.out.println("Acesso bloqueado");
+            }
         }
-        System.out.println("Acesso Permitido");
+        System.out.println("Acesso aprovado");
+
+
         sc.close();
     }
 }
