aeb3b55fc4c23b356af7261bb4dbe6737246409c
Tiago Chacon <tiagochacon80@gmail.com>
2022-09-24 15:13:44 -0400

    Java logic challenge

--- src/EntradaESaidaCPF.java
+++ src/EntradaESaidaCPF.java
@@ -1,14 +1,28 @@
+// Entrada e Saída CPF
+
+/*
+Elabore um programa que possuas as características abaixo:
+1. Leia os dados de um CPF no formato XXX.YYY.ZZZ-DD;
+2. Imprima os quatro grupos numéricos, sendo um valor por linha.
+- Entrada
+A entrada consiste vários arquivos de teste e cada um possuindo uma linha com
+formato XXX.YYY.ZZZ-DD, onde XXX, YYY, ZZZ, DD são números inteiros.
+- Saída
+Para cada arquivo da entrada, tem que ter um arquivo de saída com quatro linhas,
+e em cada linha um número inteiro de acordo com procedimento 2 descrito no
+Desafio. Confira o exemplo abaixo:
+*/
+import java.util.Locale;
 import java.util.Scanner;
 
 public class EntradaESaidaCPF {
 
     public static void main(String[] args) {
-
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
         String[] cpf = new String[4];
         cpf = sc.nextLine().split("-");
-        System.out.println(cpf[0]);
-
+        
         for (int i = 0; i< cpf.length; i++) {
           System.out.println(cpf[i].replace(".", "\n"));
         }
