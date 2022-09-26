d0ffda9fad78cc408340c5e93392bc7ca94d9f89
Tiago Chacon <tiagochacon80@gmail.com>
2022-09-26 15:40:42 -0400

    Java logic challenge

--- src/EntradaESaidaCPF.java
+++ src/EntradaESaidaCPF.java
@@ -22,7 +22,7 @@ public class EntradaESaidaCPF {
         Scanner sc = new Scanner(System.in);
         String[] cpf = new String[4];
         cpf = sc.nextLine().split("-");
-        
+
         for (int i = 0; i< cpf.length; i++) {
           System.out.println(cpf[i].replace(".", "\n"));
         }
