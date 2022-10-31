ce1bb80187a891758e7b6c6ce50c974626155932
Tiago Chacon <tiagochacon80@gmail.com>
2022-10-31 09:47:12 -0400

    Java logic exercices

---
+++ src/URI1007.java
@@ -0,0 +1,25 @@
+/*
+Leia quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).
+Entrada
+O arquivo de entrada contém 4 valores inteiros.
+Saída
+Imprima a mensagem DIFERENCA com todas as letras maiúsculas, conforme exemplo abaixo, com um espaço em branco antes e depois da igualdade.
+*/
+import java.util.Scanner;
+
+public class URI1007 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        int diferenca = (a * b - c * d);
+
+        System.out.println("DIFERENÇA = " + diferenca);
+
+        sc.close();
+    }
+}
