5db8998c40d32ad756b4242e36bdab1a8ec12e26
Tiago Chacon <tiagochacon80@gmail.com>
2022-11-01 17:57:49 -0400

    Java logic exercices

---
+++ src/URI1017.java
@@ -0,0 +1,33 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+/*
+Joaozinho quer calcular e mostrar a quantidade de litros de combustível gastos em uma viagem, ao utilizar um automóvel que faz 12 KM/L.
+Para isso, ele gostaria que você o auxiliasse através de um simples programa.
+Para efetuar o cálculo, deve-se fornecer o tempo gasto na viagem (em horas) e a velocidade média durante a mesma (em km/h).
+Assim, pode-se obter distância percorrida e, em seguida, calcular quantos litros seriam necessários. Mostre o valor com 3 casas decimais após o ponto.
+
+Entrada
+O arquivo de entrada contém dois inteiros. O primeiro é o tempo gasto na viagem (em horas) e o segundo é a velocidade média durante a mesma (em km/h).
+
+Saída
+Imprima a quantidade de litros necessária para realizar a viagem, com três dígitos após o ponto decimal
+ */
+public class URI1017 {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int automovel = 12;
+        int viagem = sc.nextInt();
+        int velocidade = sc.nextInt();
+
+        double quantidade = (double) viagem * velocidade / automovel;
+
+        System.out.printf("%.3f%n", quantidade);
+
+        sc.close();
+    }
+}
