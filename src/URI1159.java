5ec57f1d51c89b551af15c571a1ba259dfeaefab
Tiago Chacon <tiagochacon80@gmail.com>
2022-11-25 11:47:16 -0500

    exercice logic in Java

--- src/URI1159.java
+++ src/URI1159.java
@@ -19,7 +19,7 @@ public class URI1159 {
             if (x % 2 != 0) {
                 x += 1;
             }
-            soma = x + x + 2 + x + 4 + x + 6 + x + 8;
+            soma = 5 * x + 20;
             System.out.println(soma);
 
             x = sc.nextInt();
