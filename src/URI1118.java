9ed71697367156d5a37852bdc4cbad0178500ac8
Tiago Chacon <tiagochacon80@gmail.com>
2022-11-29 12:06:46 -0500

    exercice logic in Java

---
+++ src/URI1118.java
@@ -0,0 +1,39 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class URI1118 {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int novoCalculo = 1;
+
+        while (novoCalculo == 1) {
+            double nota1 = sc.nextDouble();
+            while (nota1 < 0.0 || nota1 > 10.0) {
+                System.out.println("nota invalida");
+                nota1 = sc.nextDouble();
+            }
+
+            double nota2 = sc.nextDouble();
+            while (nota2 < 0.0 || nota2 > 10.0) {
+                System.out.println("nota invalida");
+                nota2 = sc.nextDouble();
+            }
+
+            double media = (nota1 + nota2) / 2.0;
+            System.out.printf("media = %.2f%n", media);
+
+            System.out.println("novo calculo (1-sim nao-2)");
+            novoCalculo = sc.nextInt();
+            while (novoCalculo != 1 && novoCalculo != 2) {
+                System.out.println("novo calculo (1-sim nao-2)");
+                novoCalculo = sc.nextInt();
+            }
+        }
+
+        sc.close();
+    }
+}
