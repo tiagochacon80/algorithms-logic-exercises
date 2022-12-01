d91456e4da57c830db2c394505a2749283341e25
Tiago Chacon <tiagochacon80@gmail.com>
2022-12-01 12:33:43 -0500

    exercice logic in Java

---
+++ src/URI1072.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class URI1072 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int in = 0;
+        int out = 0;
+        for (int i = 1; i <= n; i++) {
+            int x = sc.nextInt();
+            if (x >= 10 && x <= 20) {
+                in += 1;
+            } else {
+                out += 1;
+            }
+        }
+
+        System.out.println(in + " in");
+        System.out.println(out + " out");
+
+        sc.close();
+    }
+}
